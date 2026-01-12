47db728d3fdb684f6161fa3d00bca45b4b94e949
Razzx-notdev <razzxdominos@gmail.com>
2026-01-12 10:13:01 +0700

    Update engine-requirements.js

--- engine-requirements.js
+++ engine-requirements.js
@@ -1,10 +1,10 @@
 const major = parseInt(process.versions.node.split('.')[0], 10);
 
-if (major < 20) {
+if (major < 22) {
       console.error(
-    `\n❌ Paket ini membutuhkan Node.js 20+ untuk berjalan dengan baik.\n` +
+    `\n❌ Paket ini membutuhkan Node.js 22+ untuk berjalan dengan baik.\n` +
     `   Anda menggunakan Node.js ${process.versions.node}.\n` +
-    `   Silakan upgrade ke Node.js 20+ untuk melanjutkan.\n`
+    `   Silakan upgrade ke Node.js 22+ untuk melanjutkan.\n`
   );
   process.exit(1);
 }
